c1227b6d0217901e79919a6ffeaa1b92e50d18f4
dldou <99049494+dldou@users.noreply.github.com>
2026-05-09 19:55:16 +0200

    train_baseline bash to run to fix the nb of workers

---
+++ train_baseline.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -A xxx
+#SBATCH --job-name=xxx
+#SBATCH --partition=xxxx
+#SBATCH --constraint=xxx
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --output=%x_%A.out
+
+module purge
+module load arch/a100
+module load pytorch-gpu/py3/2.3.0
+module load nvidia-nsight-systems/2024.1.1.59
+
+
+export PYTHONPATH="${PYTHONPATH:-}:$(pwd)"
+export PYTHONUNBUFFERED=1
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+ln -sfn "$JOBSCRATCH" /tmp/nvidia || true
+
+WORKERS_LIST=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
+
+for NUM_WORKERS in "${WORKERS_LIST[@]}"; do
+    if [ "$NUM_WORKERS" -gt "$SLURM_CPUS_PER_TASK" ]; then
+        echo "Skip: NUM_WORKERS=$NUM_WORKERS > SLURM_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK"
+        continue
+    fi
+
+    echo "======================================"
+    echo "NUM_WORKERS=$NUM_WORKERS"
+    echo "======================================"
+
+    NUM_WORKERS="$NUM_WORKERS" srun python -u train_torch.py 
+done
