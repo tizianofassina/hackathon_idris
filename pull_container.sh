70691d8703b31a3fc3f4c55ecab7286124f75a1b
tizianofassina <tifas98@gmail.com>
2026-05-20 12:28:05 +0200

    c

--- pull_container.sh
+++ pull_container.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-#SBATCH -A ltc@h100
-#SBATCH --job-name=TRAIN_FLOW_DDP_PROF
-#SBATCH --partition=gpu_p6
-#SBATCH --constraint="h100&prof2"
+#SBATCH -A ltc@cpu
+#SBATCH --job-name=PULL_SING
+#SBATCH --partition=prepost
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=16
-#SBATCH --exclusive
+#SBATCH --cpus-per-task=2
 #SBATCH --time=01:00:00
 #SBATCH --output=%x_%A_%a.out
-#SBATCH --array=0-5
 
 
+module load singularity/3.8.5  
 
 singularity pull transformer_engine.sif docker://nvcr.io/nvidia/pytorch:26.02-py3
